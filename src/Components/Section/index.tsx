c14ef3c63d6c13fb75282b1655e6061d9828e68b
joaorocha <joaoeduardorrocha@gmail.com>
2024-02-19 20:12:47 -0300

    feat: Add Styles Section Component

--- src/Components/Section/index.tsx
+++ src/Components/Section/index.tsx
@@ -5,92 +5,92 @@ import Project from '../Project'
 const Section: React.FC = () => {
 
   return (
-    <section>
-      <h3>
+    <section className="section-content">
+      <h3 className="section-content__title-about-me">
         Sobre mim
       </h3>
-      <div>
-        <p>
+      <div className="section-content__about-me">
+        <p className="section-content__about-me__paragraph">
           Com 24 anos de idade, mergulhei no mundo da programação.
           Desde então, cada linha de código se tornou uma fonte de fascínio,
           transformando-se em aplicações funcionais e impactantes.
         </p>
-        <p>
+        <p className="section-content__about-me__paragraph">
           A pandemia trouxe mudanças significativas, e aproveitei a oportunidade
           para direcionar totalmente minha dedicação para a área de desenvolvimento.
           Atualmente, sou um Desenvolvedor Front-End, especializado nas tecnologias React e TypeScript
         </p>
       </div>
 
-      <h3>
+      <h3 className="section-content__title-education">
         Formação
       </h3>
 
-      <div>
-        <p>
+      <div className="section-content__education">
+        <p className="section-content__education__legend">
           Trilha Explorer - Programa de Aprendizado Progressivo da Rocketseat
         </p>
-        <p>
+        <p className="section-content__education__institution">
           Instituição: Rocketseat
         </p>
-        <p>
+        <p className="section-content__education__conclusion">
           Conclusão: outubro/2023;
         </p>
 
-        <details className='.custom-details'>
+        <details className='section-content__details'>
           <summary>Saiba Mais</summary>
           <ul>
-            <legend>Tópicos Abordados:</legend>
-            <li>Fundamentos de programação e lógica de programação</li>
-            <li>Desenvolvimento web com HTML, CSS e JavaScript</li>
-            <li>Introdução ao ReactJS e desenvolvimento de interfaces web interativas</li>
-            <li>Introdução ao Node.js e construção de APIs RESTful</li>
-            <li>Banco de dados com MongoDB e Mongoose</li>
-            <li>Versionamento de código com Git e GitHub</li>
-            <li>Metodologias ágeis e boas práticas de desenvolvimento de software</li>
+            <legend className='section-content__details__summary-title'>Tópicos Abordados:</legend>
+            <li>- Fundamentos de programação e lógica de programação</li>
+            <li>- Desenvolvimento web com HTML, CSS e JavaScript</li>
+            <li>- Introdução ao ReactJS e desenvolvimento de interfaces web interativas</li>
+            <li>- Introdução ao Node.js e construção de APIs RESTful</li>
+            <li>- Banco de dados com MongoDB e Mongoose</li>
+            <li>- Versionamento de código com Git e GitHub</li>
+            <li>- Metodologias ágeis e boas práticas de desenvolvimento de software</li>
           </ul>
         </details>
       </div>
 
-      <div>
-        <p>
+      <div className="section-content__education">
+        <p className="section-content__education__legend">
           Introdução à Ciência da Computação
         </p>
-        <p>
+        <p className="section-content__education__institution">
           Instituição: Fundação Escolar (Curso de Harvard)
         </p>
-        <p>
+        <p className="section-content__education__conclusion">
           Conclusão: fevereiro/2023;
         </p>
 
-        <details className='.custom-details'>
+        <details className='section-content__details'>
           <summary>Saiba Mais</summary>
           <ul>
-            <legend>Tópicos Abordados:</legend>
-            <li>Fundamentos de programação e lógica de programação</li>
-            <li>Estruturas de dados e algoritmos</li>
-            <li>Introdução à linguagem de programação C</li>
-            <li>Desenvolvimento web com HTML, CSS e JavaScript</li>
-            <li>Pensamento computacional e resolução de problemas</li>
+            <legend className='section-content__details__summary-title'>Tópicos Abordados:</legend>
+            <li>- Fundamentos de programação e lógica de programação</li>
+            <li>- Estruturas de dados e algoritmos</li>
+            <li>- Introdução à linguagem de programação C</li>
+            <li>- Desenvolvimento web com HTML, CSS e JavaScript</li>
+            <li>- Pensamento computacional e resolução de problemas</li>
           </ul>
         </details>
       </div>
 
-      <div>
-        <p>
-          Curso de Introdução à Tecnologia
+      <div className="section-content__education">
+        <p className="section-content__education__legend">
+          Introdução à Tecnologia
         </p>
-        <p>
+        <p className="section-content__education__institution">
           Instituição: Rocketseat
         </p>
-        <p>
+        <p className="section-content__education__conclusion">
           Conclusão: junho/2022;
         </p>
 
-        <details className='.custom-details'>
+        <details className='section-content__details'>
           <summary>Saiba Mais</summary>
           <ul>
-            <legend>Tópicos Abordados:</legend>
+            <legend className='section-content__details__summary-title'>Tópicos Abordados:</legend>
             <li>Fundamentos da Programação</li>
             <li>Desenvolvimento web com HTML, CSS e JavaScript</li>
             <li>Introdução ao ReactJS</li>
