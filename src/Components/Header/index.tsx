a89ee7f7adb96fadc93ba563776f5621f101eed5
joaorocha <joaoeduardorrocha@gmail.com>
2024-02-21 13:59:49 -0300

    feature: Add Links

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -12,30 +12,42 @@ const Header: React.FC = () => {
       <h2 className='header-content__subtitle'>Desenvolvedor React Front-end</h2>
 
       <div className='header-content__social-icons'>
-        <div className='header-content__social-icons-margin'>
+        <a className='header-content__social-icons-margin'
+          href='https://www.linkedin.com/in/joaoedrocha/'
+          target="_blank"
+          rel="noreferrer">
           <FaLinkedin
             size={35}
             className='linkedin-icon'
           />
-        </div>
-        <div className='header-content__social-icons-margin'>
+        </a>
+        <a className='header-content__social-icons-margin'
+          href='https://github.com/JoaoEduardoRRocha'
+          target="_blank"
+          rel="noreferrer">
           <FaGithubSquare
             size={35}
             className='github-icon'
           />
-        </div>
-        <div className='header-content__social-icons-margin'>
+        </a>
+        <a className='header-content__social-icons-margin'
+          href='https://api.whatsapp.com/send/?phone=5521976747042&text&type=phone_number&app_absent=0'
+          target="_blank"
+          rel="noreferrer">
           <FaWhatsappSquare
             size={35}
             className='whatsapp-icon'
           />
-        </div>
-        <div className='header-content__social-icons-margin'>
+        </a>
+        <a className='header-content__social-icons-margin'
+          href='https://drive.google.com/file/d/1vUZBDba9TtnSUgfuw1A_I2RYGNbayeJV/view'
+          target="_blank"
+          rel="noreferrer">
           <BsArrowDownSquareFill
             size={32}
             className='file-icon'
           />
-        </div>
+        </a>
       </div>
     </header>
   );
